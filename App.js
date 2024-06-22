75be9238404a919f6ad0c4eaa94ac8920c334332
Blessen George <blessen.george30@gmail.com>
2024-06-22 13:37:53 +0530

    yee

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
 
       <View style={styles.inputContainer}>
         <TextInput style={styles.textInput}
-          placeholder='Your course goalllllllll! '
+          placeholder='Your course goalllllll! '
           value={enteredGoalText}
           onChangeText={goalInputHandler} />
         <Button title="Add Goal" onPress={addGoalhandler}/>
